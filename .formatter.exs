e6117b313976f955d8407aeb2530e11bb02f665f
Jinkyou Son <nallwhy@gmail.com>
2025-03-21 14:55:43 +0900

    Add ash_ai to deps

--- .formatter.exs
+++ .formatter.exs
@@ -1,5 +1,14 @@
 [
-  import_deps: [:ash_paper_trail, :ash_archival, :ash_postgres, :ash, :ecto, :ecto_sql, :phoenix],
+  import_deps: [
+    :ash,
+    :ash_postgres,
+    :ash_archival,
+    :ash_paper_trail,
+    :ash_ai,
+    :ecto,
+    :ecto_sql,
+    :phoenix
+  ],
   subdirectories: ["priv/*/migrations"],
   plugins: [Spark.Formatter, Phoenix.LiveView.HTMLFormatter],
   inputs: ["*.{heex,ex,exs}", "{config,lib,test}/**/*.{heex,ex,exs}", "priv/*/seeds.exs"]
--- mix.exs
+++ mix.exs
@@ -39,6 +39,7 @@ defmodule AshPg.MixProject do
       {:ash_postgres, "~> 2.0"},
       {:ash_archival, "~> 1.0"},
       {:ash_paper_trail, "~> 0.5"},
+      {:ash_ai, github: "ash-project/ash_ai", branch: "main"},
       {:phoenix, "~> 1.7.18"},
       {:phoenix_ecto, "~> 4.5"},
       {:ecto_sql, "~> 3.10"},
