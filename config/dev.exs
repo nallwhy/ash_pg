64c2347fccb05061087801196f709edadc3e739d
Jinkyou Son <nallwhy@gmail.com>
2025-03-25 19:03:28 +0900

    Don't truncate in dev

--- config/dev.exs
+++ config/dev.exs
@@ -68,6 +68,7 @@ config :ash_pg, dev_routes: true
 
 # Do not include metadata nor timestamps in development logs
 config :logger, :console, format: "[$level] $message\n"
+config :logger, truncate: :infinity
 
 # Set a higher stacktrace during development. Avoid configuring such
 # in production as building large stacktraces may be expensive.
